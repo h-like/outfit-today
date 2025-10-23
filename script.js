a4cf0d53144ecafa3aab214371eb0fe9a3c763a4
ilililillliiii <hyoeunlike@naver.com>
2025-10-24 00:40:02 +0900

    function updated

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-const http = new XMLHttpRequest();
-let result = document.querySelector("#result");
+const http = new XMLHttpRequest()
+let result = document.querySelector("#result")
+
+document.querySelector("#share").addEventListener("click", () => {
+  findMyCoordinates()
+})
+
+function findMyCoordinates() {
+
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition((position) => {
+       console.log(position.coords.latitude, position.coords.longitude)
+    },
+    (err) => { 
+      alert(err.message)
+    })
+  } else {
+    alert("Geolocation is not supported by your browser")
+  }
+
+}
